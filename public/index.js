c35ec8b96daa349e5514e9f64019d82dd5d5e654
BURDET ETIENNE <Etienne.BURDET@arep.fr>
2019-07-16 17:56:31 +0200

    Vue listing

---
+++ public/index.js
@@ -0,0 +1,22 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    count: [
+      {
+        place: "Escalier A",
+        up: 10,
+        down: 5
+      },
+      {
+        place: "Hall",
+        up: 10,
+        down: 5
+      },
+      {
+        place: "PASO sud",
+        up: 10,
+        down: 5
+      }
+    ]
+  }
+})
