dbe8c20264b08fff2774f61c7f1edd7d7386bb21
BURDET ETIENNE <Etienne.BURDET@arep.fr>
2019-07-17 16:04:20 +0200

    Push form to table

--- public/index.js
+++ public/index.js
@@ -1,22 +1,43 @@
+
+const presetCounts = [
+  {
+    place: "Escalier A",
+    up: 10,
+    down: 5
+  },
+  {
+    place: "Hall",
+    up: 10,
+    down: 5
+  },
+  {
+    place: "PASO sud",
+    up: 10,
+    down: 5
+  }
+]
+
 var app = new Vue({
   el: '#app',
   data: {
-    count: [
-      {
-        place: "Escalier A",
-        up: 10,
-        down: 5
-      },
-      {
-        place: "Hall",
-        up: 10,
-        down: 5
-      },
-      {
-        place: "PASO sud",
-        up: 10,
-        down: 5
+    counts: presetCounts,
+    counterUp: 0,
+    counterDown: 0,
+    place: ''
+  },
+  methods: {
+    newCount: function(event) {
+      const count = {
+        place: this.place,
+        up: this.counterUp,
+        down: this.counterDown
       }
-    ]
+
+      presetCounts.push(count);
+
+      this.place = '';
+      this.counterUp = 0;
+      this.counterDown = 0;
+    }
   }
 })
