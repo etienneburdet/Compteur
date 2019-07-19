94f9e6c53825edc25b6a2db0f6866a4676de8b50
BURDET ETIENNE <Etienne.BURDET@arep.fr>
2019-07-19 16:56:18 +0200

    Read preset list from pouchDB

--- public/index.js
+++ public/index.js
@@ -1,26 +1,42 @@
 
+const db = new PouchDB('comptages');
+
 const presetCounts = [
   {
+    _id: "1",
     place: "Escalier A",
     up: 10,
     down: 5
   },
-  {
+{
+    _id: "2",
     place: "Hall",
     up: 10,
     down: 5
   },
-  {
+{
+    _id: "3",
     place: "PASO sud",
     up: 10,
     down: 5
   }
 ]
 
+db.bulkDocs(presetCounts);
+
+function fetchAndRenderAllDocs() {
+  return db.allDocs({include_docs: true}).then(function (res) {
+    const docs = res.rows.map(function (row) { return row.doc; });
+    app.counts = docs;
+  }).catch(console.log.bind(console));
+}
+
+fetchAndRenderAllDocs();
+
 var app = new Vue({
   el: '#app',
   data: {
-    counts: presetCounts,
+    counts: '',
     counterUp: 0,
     counterDown: 0,
     place: ''
